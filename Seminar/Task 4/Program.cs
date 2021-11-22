a8c1c0f427c62a381e832e62705efe00ebbca24b
Полина Брагина <polyandriys14@mail.ru>
2021-11-22 11:30:30 +0300

    Task 4

---
+++ Seminar/Task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+Console.Write("Введите номер дня недели:");
+
+int Dayofweek = int.Parse(Console.ReadLine());
+
+if (Dayofweek > 7 || Dayofweek < 1)
+{
+    Console.Write("Неверный номер дня недели");
+}
+else
+{
+Console.WriteLine(array [Dayofweek-1]);
+}
