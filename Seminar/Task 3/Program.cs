7b5b07b14b879513907c133e2276783c86a14da3
Полина Брагина <polyandriys14@mail.ru>
2021-11-22 10:46:47 +0300

    Task 3

---
+++ Seminar/Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write("Num2 большее число, Num1 меньшее число");
+}
+else
+{
+Console.Write("Num1 большее число, Num2 меньшее число");
+}
