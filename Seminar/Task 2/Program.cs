8c8c4eda6adff52be82e7cdd450ab5d69bde78ac
Полина Брагина <polyandriys14@mail.ru>
2021-11-22 10:42:39 +0300

    Task 2

---
+++ Seminar/Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+if (num2 == num1* num1)
+{
+    Console.Write("Num2 это квадрат Num1");
+}
+else
+{
+Console.Write("Num2 это не квадрат Num1");
+}
