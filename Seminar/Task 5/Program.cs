4fba2d7118ba062d2220a769e13982228cfa7828
Полина Брагина <polyandriys14@mail.ru>
2021-11-22 11:48:33 +0300

    Task 5

---
+++ Seminar/Task 5/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(1,11);
+Console.WriteLine(a);
+int b = new Random().Next(1,11);
+Console.WriteLine(b);
+int c = new Random().Next(1,11);
+Console.WriteLine(c);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
