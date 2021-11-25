08501440fc95ebd7a0d47190b4a73f2762f13bbc
Полина Брагина <polyandriys14@mail.ru>
2021-11-25 11:07:00 +0300

    Task 8

---
+++ Seminar/Task 8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+int numP = new Random().Next(1,10);
+Console.WriteLine(numP);
+
+int numN = -numP;
+
+
+while (numN <= numP)
+{
+   Console.WriteLine(numN);
+numN = numN + 1;
+}
