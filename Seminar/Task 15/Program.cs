2095a9284689874c1cd98801cc1d25d9561a1e61
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 10:57:07 +0300

    task 15

---
+++ Seminar/Task 15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int N = new Random().Next(1,999);
+Console.WriteLine(N);
+if (N > 99)
+{
+    Console.WriteLine($"Третья цифра числа: {N%10}");
+}
+else
+{
+Console.WriteLine($"В этом числе нет третьей цифры");
+}
