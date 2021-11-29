7deafea8284d281f7326cb5b4fb0315256ac01be
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 10:49:30 +0300

    task 14

---
+++ Seminar/Task 14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int N = new Random().Next(1,100);
+Console.WriteLine(N);
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if (N%num == 0)
+{
+    Console.WriteLine($"Число {N} кратно числу {num}");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления: {N%num}");
+}
