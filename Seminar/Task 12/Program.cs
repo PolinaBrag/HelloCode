287a83a46a3cb3cd7332cac5228144cb49bfeb7d
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 10:03:01 +0300

    task 12

--- Seminar/Task 12/Program.cs
+++ Seminar/Task 12/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 int N = new Random().Next(10,99);
 Console.WriteLine(N);
+int FirstNumber = N %100;
+FirstNumber = (FirstNumber/10);
+
+int SecondNumber = N %10;
+
+if (FirstNumber > SecondNumber)
+{
+    Console.WriteLine(FirstNumber);
+}
+else
+{
+Console.WriteLine(SecondNumber);
+}
