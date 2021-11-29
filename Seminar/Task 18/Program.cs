e6a00b2bf1e702ab344e839d00680f07e1084760
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 22:37:28 +0300

    task 18

---
+++ Seminar/Task 18/Program.cs
@@ -0,0 +1,21 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine($"Число {num2} является квадратом числа {num1}");
+}
+else
+{
+if (num1 == num2 * num2)
+{
+     Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Заданные числа не являются квадратом друг друга");
+}
+}
