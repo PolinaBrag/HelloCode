83353264400eeb299d42672c136156d419978c1b
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 11:06:44 +0300

    task 16

---
+++ Seminar/Task 16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int N = new Random().Next(1,999);
+Console.WriteLine(N);
+int FirstNumber = 7;
+int SecondNumber = 23;
+if (N%FirstNumber == 0)
+{
+    Console.WriteLine($"Число {N} кратно числу {FirstNumber}");
+}
+else
+{
+    Console.WriteLine($"Число {N} не кратно числу {FirstNumber}");
+}
+
+if (N%SecondNumber == 0)
+{
+    Console.WriteLine($"Число {N} кратно числу {SecondNumber}");
+}
+else
+{
+    Console.WriteLine($"Число {N} не кратно числу {SecondNumber}");
+}
