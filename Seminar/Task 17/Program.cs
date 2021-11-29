f3d9be4574dcdaa48ebdb8848a934aea9546fffe
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 11:58:18 +0300

    task 17

--- Seminar/Task 17/Program.cs
+++ Seminar/Task 17/Program.cs
@@ -1,1 +1,28 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+Console.Write("Введите номер дня недели:");
+
+int Dayofweek = int.Parse(Console.ReadLine());
+    
+
+  if (Dayofweek < 1 || Dayofweek > 7 )
+    {
+    Console.WriteLine("В неделе 7 дней");
+    }
+else
+{
+if (Dayofweek == 6 || Dayofweek == 7)
+    {
+     Console.WriteLine("День недели выходной");
+    }
+  
+else
+    {
+    Console.WriteLine("День недели будний");
+    }
+
+}
+
+
+
