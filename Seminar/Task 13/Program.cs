c77fc4afcba98b7f618654d2c9a107bd237b5384
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 10:38:34 +0300

    task 13

---
+++ Seminar/Task 13/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int N = new Random().Next(100,999);
+Console.WriteLine(N);
+int FirstNumber = N/100;
+int ThirdNumber = N%10;
+Console.Write(FirstNumber);
+Console.Write(ThirdNumber);
