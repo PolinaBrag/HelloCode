442d1fa660f1d4260354c1aca3ea974a026ee962
Полина Брагина <polyandriys14@mail.ru>
2021-11-29 11:16:00 +0300

    task 16 new

--- Seminar/Task 16/Program.cs
+++ Seminar/Task 16/Program.cs
@@ -3,20 +3,12 @@ int N = new Random().Next(1,999);
 Console.WriteLine(N);
 int FirstNumber = 7;
 int SecondNumber = 23;
-if (N%FirstNumber == 0)
+if (N%FirstNumber == 0 && N%SecondNumber == 0)
 {
-    Console.WriteLine($"Число {N} кратно числу {FirstNumber}");
+    Console.WriteLine($"Число {N} кратно числу {FirstNumber} и {SecondNumber}");
 }
 else
 {
-    Console.WriteLine($"Число {N} не кратно числу {FirstNumber}");
+    Console.WriteLine($"Число {N} не кратно числу {FirstNumber} и {SecondNumber}");
 }
 
-if (N%SecondNumber == 0)
-{
-    Console.WriteLine($"Число {N} кратно числу {SecondNumber}");
-}
-else
-{
-    Console.WriteLine($"Число {N} не кратно числу {SecondNumber}");
-}
