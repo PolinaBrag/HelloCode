bf129f81a019f1df2870c9c7a3338ee6c4add742
Полина Брагина <polyandriys14@mail.ru>
2021-12-06 11:31:31 +0300

    task 22

---
+++ Seminar/Task 22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите пятизначное число:");
+int Palindrom = int.Parse(Console.ReadLine());
+int[] Array = new int [5];
+
+for (int i = 0; i <= 4; i++)
+{
+    Array[i] = Palindrom%10;
+    Palindrom = Palindrom/10;
+}
+int j = 0;
+while (j < 2)
+{
+    if(Array[j] != Array[4-j])
+    {
+        Console.WriteLine("Число не является палиндромом");
+        return;
+    }
+    
+       j++; 
+    
+    
+}
+Console.WriteLine("Число является палиндромом"); 
