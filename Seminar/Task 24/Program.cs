690b8b8429f5f3d2710fbd48f85c47b26781f76c
Полина Брагина <polyandriys14@mail.ru>
2021-12-06 13:10:48 +0300

    task 24

--- Seminar/Task 24/Program.cs
+++ Seminar/Task 24/Program.cs
@@ -1,2 +1,11 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
-int[] = new int ()
+
+    for (int i = 2; i <= 10; i++)
+    {
+        for (int j = 2; j <= 10; j++)
+        {
+            Console.WriteLine($"{i} * {j} = {i * j}");
+        }
+         Console.WriteLine();
+    }
+
