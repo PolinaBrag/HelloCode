3e3f85264379f1d2ca7decf8c7a066783925fba7
Полина Брагина <polyandriys14@mail.ru>
2021-12-06 13:34:03 +0300

    task 26

---
+++ Seminar/Task 26/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+
+int j = 45;
+int total = 0;
+
+for (int i = 1; i <= j; i++)
+{
+    total = total + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {j} = {total}");
