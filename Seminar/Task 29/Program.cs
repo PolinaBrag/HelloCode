39464a1a7f091c1cf1f2592f471ee6950fa67c22
Полина Брагина <polyandriys14@mail.ru>
2021-12-09 11:17:58 +0300

    task 29

---
+++ Seminar/Task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+int Num = new Random().Next(1, 100000);
+Console.WriteLine($"Our number is {Num}");
+
+int Sum = 0;
+int A = Num;
+
+while (Num > 0)
+{
+    A = Num%10;
+    Sum += A;
+    Num = Num/10;
+}
+Console.WriteLine($"Sum of digits is {Sum}");
