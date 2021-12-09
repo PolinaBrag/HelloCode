54f0a9f1c268709cf67eb8bf8e53e59efe5e7f52
Полина Брагина <polyandriys14@mail.ru>
2021-12-09 11:36:13 +0300

    task 31

---
+++ Seminar/Task 31/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+int N = new Random().Next(1,100);
+Console.WriteLine($"Our number is {N}");
+
+for (int i = 1; i <= N; i++)
+{
+    if(i%2 == 0)
+    {
+        Console.WriteLine($"Куб числа {i} = {Math.Pow(i, 3)}");
+    }
+}
