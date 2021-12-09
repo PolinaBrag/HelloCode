9dd825b98746b596bab99c9bac5b819f83f2558d
Полина Брагина <polyandriys14@mail.ru>
2021-12-09 11:00:14 +0300

    task 28

---
+++ Seminar/Task 28/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить количество цифр в числе
+
+double Num = new Random().Next(1, 10000);
+Console.WriteLine($"Our number is {Num}");
+int count = 0;
+
+while (Num > 10)
+{
+Num = Num/10;
+count++;
+}
+Console.WriteLine($"Amount of digits is {count+1}");
