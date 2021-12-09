e08a3b24d878cc5f81d08cdf68975e9430554f8d
Полина Брагина <polyandriys14@mail.ru>
2021-12-09 11:26:22 +0300

    task 30

---
+++ Seminar/Task 30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int N = new Random().Next(1,10);
+Console.WriteLine($"Our number is {N}");
+
+double Product = 1;
+
+for(int i = 1; i <= N; i++)
+{
+    Product = Product * i;
+}
+Console.WriteLine($"Product of number is {Product}");
