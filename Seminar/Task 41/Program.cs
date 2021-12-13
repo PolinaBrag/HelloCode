2f6ed20807e756507cb5bbecc8bdfc2f18625b50
Полина Брагина <polyandriys14@mail.ru>
2021-12-13 23:18:12 +0300

    task 41

---
+++ Seminar/Task 41/Program.cs
@@ -0,0 +1,43 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double[] array = new double[10];
+
+void FillArray(double[] arrayFirst)
+{
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        arrayFirst[i] = new Random().Next(-50 , 50);
+    }
+}
+
+void PrintArray(double[] arraySecond)
+{
+    for (int i = 0; i < arraySecond.Length; i++)
+    {
+        Console.Write(arraySecond[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(array);
+PrintArray(array);
+
+double max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"Максимальное число {max}");
+
+double min = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"Минимальное число {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом равна {max - min}");
