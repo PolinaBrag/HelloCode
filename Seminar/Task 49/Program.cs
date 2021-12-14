7df10b962dca754719d30239c8ebb2ddc66ffe8c
Полина Брагина <polyandriys14@mail.ru>
2021-12-14 18:04:36 +0300

    task 49

---
+++ Seminar/Task 49/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+int[,] matrix = new int[3,3];
+
+void FillAray(int[,] matrix2)
+{
+    for (int row = 0; row < matrix2.GetLength(0); row++)
+    {
+      for (int column = 0; column < matrix2.GetLength(1); column++)
+      {
+        matrix2[row, column] = new Random().Next(1,100);
+        Console.Write(matrix2[row, column] + " ");
+      }
+      Console.WriteLine();
+    }
+}
+
+FillAray(matrix);
