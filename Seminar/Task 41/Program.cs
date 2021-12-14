7907b97c9c4deb2cdfe6671b14c85d576ea6f829
Полина Брагина <polyandriys14@mail.ru>
2021-12-14 15:59:25 +0300

    task 4

--- Seminar/Task 41/Program.cs
+++ Seminar/Task 41/Program.cs
@@ -5,39 +5,29 @@ void FillArray(double[] arrayFirst)
 {
     for (int i = 0; i < arrayFirst.Length; i++)
     {
-        arrayFirst[i] = new Random().Next(-50 , 50);
-    }
-}
-
-void PrintArray(double[] arraySecond)
-{
-    for (int i = 0; i < arraySecond.Length; i++)
-    {
-        Console.Write(arraySecond[i] + " ");
+        arrayFirst[i] = new Random().NextDouble() + new Random().Next(-50, 50);
+        Console.Write(arrayFirst[i] + " ");
     }
     Console.WriteLine();
 }
 
 FillArray(array);
-PrintArray(array);
 
-double max = 0;
+double max = array[0];
+double min = array[0];
+
 for (int i = 0; i < array.Length; i++)
 {
-    if(array[i] > max)
+    if (array[i] > max)
     {
         max = array[i];
     }
-}
-Console.WriteLine($"Максимальное число {max}");
-
-double min = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if(array[i] < min)
+    else if (array[i] < min)
     {
         min = array[i];
     }
+
 }
+Console.WriteLine($"Максимальное число {max}");
 Console.WriteLine($"Минимальное число {min}");
 Console.WriteLine($"Разница между максимальным и минимальным элементом равна {max - min}");
