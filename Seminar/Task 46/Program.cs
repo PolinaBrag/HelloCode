eb91824c8656517f0278511224456fe0602b711a
Полина Брагина <polyandriys14@mail.ru>
2021-12-14 23:40:47 +0300

    task 46

---
+++ Seminar/Task 46/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа Фибоначчи
+
+Console.WriteLine("Ввведите число, заканчивающее ряд Фибоначчи:");
+int end = int.Parse(Console.ReadLine());
+
+int firstNum = 1;
+int secondNum = 1;
+Console.Write(firstNum + " " + secondNum + " ");
+
+for (int i = 1; i <= end; i++)
+{
+    int fibNum = firstNum + secondNum;
+    Console.Write(fibNum + " ");
+    firstNum = secondNum;
+    secondNum = fibNum;
+}
