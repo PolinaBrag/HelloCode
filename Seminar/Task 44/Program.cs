9c78b39cf5e969461e890451a08aa0716ebe0349
Полина Брагина <polyandriys14@mail.ru>
2021-12-14 23:06:01 +0300

    task 45

---
+++ Seminar/Task 44/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+Console.WriteLine("Введите десятичное число:");
+int num = int.Parse(Console.ReadLine());
+
+int answer = 0;
+int count = 0;
+//int[] binaryNumber = {};
+
+while(num != 0)
+{
+answer = num %2;
+//binaryNumber[count] = answer;
+num = num/2;
+count++;
+Console.Write(answer);
+}
+
+
+// for (int i = binaryNumber.Length-1; i >= 0; i--)
+// {
+//     
+//     Console.Write(binaryNumber[binaryNumber.Length - 1]);
+// }
+
+
