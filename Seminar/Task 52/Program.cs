39b4818ff351b67eb643b559827d2bf0d2bc83e8
Полина Брагина <polyandriys14@mail.ru>
2021-12-14 18:41:52 +0300

    task 52

---
+++ Seminar/Task 52/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] matrix = new int[6,6];
+
+void FillAray(int[,] matrix2)
+{
+    for (int row = 0; row < matrix2.GetLength(0); row++)
+    {
+      for (int column = 0; column < matrix2.GetLength(1); column++)
+      {
+        matrix2[row, column] = row + column;
+        Console.Write(matrix2[row, column] + " ");
+      }
+      Console.WriteLine();
+    }
+}
+
+FillAray(matrix);
