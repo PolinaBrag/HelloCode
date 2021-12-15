0b0b4fb4a8b1e6b3e73af2f27e5c068607474fee
Полина Брагина <polyandriys14@mail.ru>
2021-12-15 14:54:20 +0300

    task 54

--- Seminar/Task 54/Program.cs
+++ Seminar/Task 54/Program.cs
@@ -20,19 +20,37 @@ FillArray(doubleArray);
 Console.WriteLine("Введите ваше число для проверки наличия в массиве:");
 int num = int.Parse(Console.ReadLine());
 
-//void CheckNumber(int[,] newArray)
-//{
-    for (int row = 0; row < doubleArray.GetLength(0); row++)
+for (int row = 0; row < doubleArray.GetLength(0); row++)
+{
+    for (int column = 0; column < doubleArray.GetLength(1); column++)
     {
-        for (int column = 0; column < doubleArray.GetLength(1); column++)
+        if (num == doubleArray[row, column])
         {
-            if (num == doubleArray[row, column])
-            Console.WriteLine($"Число присутствует в массиве на позиции [{row} , {column}]");
+        Console.WriteLine($"Число присутствует в массиве на позиции [{row} , {column}]");
         }
     }
-    Console.WriteLine($"Данного числа нет в массиве");
-//}
+}
+Console.WriteLine($"Данного числа нет в массиве");
+
+// int row = 0;
+// int column = 0;
+
+// while (row < doubleArray.GetLength(0) && column < doubleArray.GetLength(1))
+// {
+//     if (num == doubleArray[row, column])
+//     {
+//       Console.WriteLine($"Число присутствует в массиве на позиции [{row} , {column}]");
+//        row++;
+//         column++;
+//     }
+//     else if (num != doubleArray[row, column])
+//     {
+        
+//         row++;
+//         column++;
+//     }
+// }
+
+//  Console.WriteLine($"Данного числа нет в массиве");
 
 
-
-//CheckNumber(doubleArray);
