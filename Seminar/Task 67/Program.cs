f93ced9c1be57ca4684e84ae595459911286a485
Полина Брагина <polyandriys14@mail.ru>
2021-12-15 23:04:05 +0300

    task 67

---
+++ Seminar/Task 67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int n = 20;
+
+string numbers(int n)
+{
+    if (n == 1)
+    {
+        return "" + n;
+    }
+    else
+    {
+        return numbers(n - 1) + " " + n;
+    }
+}
+
+Console.WriteLine(numbers(n));
