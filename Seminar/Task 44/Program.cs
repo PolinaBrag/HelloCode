277c5447ed8d1625f4ae15cd1b77a1f6e2b65aaa
Полина Брагина <polyandriys14@mail.ru>
2021-12-16 11:17:09 +0300

    task 44

--- Seminar/Task 44/Program.cs
+++ Seminar/Task 44/Program.cs
@@ -12,9 +12,21 @@ while(num > 0)
 answer = num %2;
 binaryNumber = answer.ToString() + binaryNumber;
 num = num/2;
-count++;
 }
 
 Console.Write(binaryNumber);
 
 
+// Console.WriteLine("Введите десятичное число:");
+// int num = int.Parse(Console.ReadLine());
+
+// int count = 0;
+// string binaryNumber = string.Empty;
+
+// while(num > 0)
+// {
+// binaryNumber = num %2 + binaryNumber;
+// num = num/2;
+// }
+
+// Console.Write(binaryNumber);
