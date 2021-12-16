e93ee5bad5c3209c86a881b5992e92b1541f0142
Полина Брагина <polyandriys14@mail.ru>
2021-12-16 11:06:31 +0300

    task 43

--- Seminar/Task 43/Program.cs
+++ Seminar/Task 43/Program.cs
@@ -1,23 +1,43 @@
 ﻿// Определить сколько чисел больше 0 введено с клавиатуры
-Console.WriteLine("Введите первое число:");
-int FirstNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
-int SecondNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число:");
-int ThirdNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите четвертое число:");
-int FouthNumber = int.Parse(Console.ReadLine());
 
-int[] array = {FirstNumber, SecondNumber, ThirdNumber, FouthNumber};
+Console.WriteLine("Сколько чисел Вы хотите ввести:");
+int countUser = int.Parse(Console.ReadLine());
 
-int count = 0;
+int countOfNumbers = 0;
 
-        for (int j = 0; j < array.Length; j++)
-        {
-            if (array[j] > 0)
-            {
-            count ++;
-            }
-        }
+for (int i = 1; i <= countUser; i++)
+{
+    Console.Write($"Число {i}: ");
+    int number = int.Parse(Console.ReadLine());
 
- Console.WriteLine($"Количество введенных чисел больше нуля равно {count}");
+if (number > 0)
+{
+    countOfNumbers++;
+
+}
+}
+Console.WriteLine($"Количество введенных чисел больше нуля равно {countOfNumbers}");
+
+
+// Console.WriteLine("Введите первое число:");
+// int firstNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int secondNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число:");
+// int thirdNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите четвертое число:");
+// int fouthNumber = int.Parse(Console.ReadLine());
+
+// int[] array = {firstNumber, secondNumber, thirdNumber, fouthNumber};
+
+// int count = 0;
+
+//         for (int j = 0; j < array.Length; j++)
+//         {
+//             if (array[j] > 0)
+//             {
+//             count ++;
+//             }
+//         }
+
+//  Console.WriteLine($"Количество введенных чисел больше нуля равно {count}");
