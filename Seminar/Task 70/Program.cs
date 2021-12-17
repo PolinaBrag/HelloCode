df6b7839dc567709a8f5463a7af99547014d0c92
Полина Брагина <polyandriys14@mail.ru>
2021-12-17 15:27:24 +0300

    task 70

---
+++ Seminar/Task 70/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int m = 5;
+int n = 35;
+int sum = 0;
+
+int numbers(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    else
+    {
+        sum = numbers(a + 1, b);
+        sum = sum + a;
+        return sum;
+    }
+}
+
+Console.WriteLine(numbers(m, n));
