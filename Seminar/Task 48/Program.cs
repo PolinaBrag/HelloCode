5b654573c30fb28eaf619e2cb14d843ea18a67d2
Полина Брагина <polyandriys14@mail.ru>
2021-12-17 14:27:32 +0300

    task 57

--- Seminar/Task 48/Program.cs
+++ Seminar/Task 48/Program.cs
@@ -1,25 +1,60 @@
 ﻿// Написать программу копирования массива
 
-Console.WriteLine("Введите количество элементов массива в виде числа:");
-int count = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество элементов массива в виде числа:");
+// int count = int.Parse(Console.ReadLine());
 
-int[] array = new int [count];
+// int[] array = new int [count];
 
-Console.WriteLine("Ваш массив:");
+// Console.WriteLine("Ваш массив:");
 
-void FillArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
+// void FillArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(1,100);
+//         Console.Write(arr[i] + " ");
+//     }
+// Console.WriteLine();
+// }
+
+// FillArray(array);
+
+// int[] newArray = new int[count];
+
+// Console.WriteLine();
+// Console.WriteLine("Копия массива:");
+
+// void CopyArray(int[] arr2)
+// {
+//     for (int i = 0; i < arr2.Length; i++)
+//     {
+//        arr2[i] = array[i];
+//        Console.Write(arr2[i] + " ");
+//     }
+// }
+
+// CopyArray(newArray);
+
+int[] array = {1,1, 4, 5, 5};
+
+for (int i = 0; i < array.Length; i++)
     {
-        arr[i] = new Random().Next(1,100);
-        Console.Write(arr[i] + " ");
+       Console.Write(array[i] + " ");
     }
+
 Console.WriteLine();
-}
 
-FillArray(array);
+string copy = string.Empty;
+for (int i = 0; i < array.Length; i++)
+    {
+       copy = copy + array[i] + " ";
+    }
 
-int[] newArray = new int[count];
+Console.WriteLine(copy + " ");
+
+
+
+int[] newArray = new int[5];
 
 Console.WriteLine();
 Console.WriteLine("Копия массива:");
---
+++ Seminar/Task 57/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+int[,] doubleArray = new int[7, 5];
+
+void FillArray(int[,] newArray)
+{
+    for (int row = 0; row < newArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < newArray.GetLength(1); column++)
+        {
+            newArray[row, column] = new Random().Next(10, 99);
+            Console.Write(newArray[row, column] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(doubleArray);
+
+Console.WriteLine();
+
+for (int row = 0; row < doubleArray.GetLength(0); row++)
+{
+    if (row == 0)
+    {
+        for (int column = 0; column < doubleArray.GetLength(1); column++)
+        {
+            int change = doubleArray[row, column];
+            doubleArray[row, column] = doubleArray[doubleArray.GetLength(0) - 1, column];
+            doubleArray[doubleArray.GetLength(0) - 1, column] = change;
+            Console.Write(doubleArray[row, column] + " ");
+        }
+            Console.WriteLine();
+    }
+    else
+    {
+        for (int column = 0; column < doubleArray.GetLength(1); column++)
+        {
+            Console.Write(doubleArray[row, column] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
