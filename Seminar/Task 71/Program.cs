959f743740449f9bd1a12429c9c84b0e800b4ee5
Полина Брагина <polyandriys14@mail.ru>
2021-12-17 15:49:19 +0300

    task 71

---
+++ Seminar/Task 71/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти сумму цифр числа
+
+int number = 100
+;
+int sum = 0;
+
+int sumOfDigits(int num)
+{
+    if (num < 10)
+    {
+        return num;
+    }
+    else
+    {
+        sum = sumOfDigits(num/10);
+        sum = sum + num %10;
+        return sum;
+    }
+}
+
+Console.WriteLine(sumOfDigits(number));
