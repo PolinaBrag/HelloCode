533267ddcf6ace58a8115fea002ae998f0ff1d8f
Полина Брагина <polyandriys14@mail.ru>
2021-12-17 14:54:35 +0300

    task 69

---
+++ Seminar/Task 69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int m = 5;
+int n = 40;
+
+void numbers(int a, int b)
+{
+    Console.Write(a + " ");
+    if (a == b)
+    {
+        return;
+    }
+    else
+    {
+        numbers(a + 1, b);
+    }
+}
+
+numbers(m, n);
