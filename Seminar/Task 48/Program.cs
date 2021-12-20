31f81352b7ef3ff98b38ced590dd6b147608b0df
Полина Брагина <polyandriys14@mail.ru>
2021-12-20 12:21:06 +0300

    task 48

--- Seminar/Task 48/Program.cs
+++ Seminar/Task 48/Program.cs
@@ -1,60 +1,25 @@
 ﻿// Написать программу копирования массива
 
-// Console.WriteLine("Введите количество элементов массива в виде числа:");
-// int count = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество элементов массива в виде числа:");
+int count = int.Parse(Console.ReadLine());
 
-// int[] array = new int [count];
+int[] array = new int [count];
 
-// Console.WriteLine("Ваш массив:");
+Console.WriteLine("Ваш массив:");
 
-// void FillArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = new Random().Next(1,100);
-//         Console.Write(arr[i] + " ");
-//     }
-// Console.WriteLine();
-// }
-
-// FillArray(array);
-
-// int[] newArray = new int[count];
-
-// Console.WriteLine();
-// Console.WriteLine("Копия массива:");
-
-// void CopyArray(int[] arr2)
-// {
-//     for (int i = 0; i < arr2.Length; i++)
-//     {
-//        arr2[i] = array[i];
-//        Console.Write(arr2[i] + " ");
-//     }
-// }
-
-// CopyArray(newArray);
-
-int[] array = {1,1, 4, 5, 5};
-
-for (int i = 0; i < array.Length; i++)
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
     {
-       Console.Write(array[i] + " ");
+        arr[i] = new Random().Next(1,100);
+        Console.Write(arr[i] + " ");
     }
-
 Console.WriteLine();
+}
 
-string copy = string.Empty;
-for (int i = 0; i < array.Length; i++)
-    {
-       copy = copy + array[i] + " ";
-    }
+FillArray(array);
 
-Console.WriteLine(copy + " ");
-
-
-
-int[] newArray = new int[5];
+int[] newArray = new int[count];
 
 Console.WriteLine();
 Console.WriteLine("Копия массива:");
@@ -68,4 +33,7 @@ void CopyArray(int[] arr2)
     }
 }
 
+
+
 CopyArray(newArray);
+
---
+++ Seminar/Task 61/Program.cs
@@ -0,0 +1,49 @@
+﻿// Составить частотный словарь элементов двумерного массива
+
+int[,] doubleArray = new int[3, 3];
+
+void FillArray(int[,] newArray)
+{
+    for (int row = 0; row < newArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < newArray.GetLength(1); column++)
+        {
+            newArray[row, column] = new Random().Next(1, 10);
+            Console.Write(newArray[row, column] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(doubleArray);
+Console.WriteLine();
+
+
+
+    // int count = 0;
+    // for (int row = 0; row < doubleArray.GetLength(0);row++)
+    // {
+    //     for (int column = 0; column < doubleArray.GetLength(1); column++)
+    //     {
+    //         for (int i = 0; i < doubleArray.GetLength(1); i++)
+    //         {
+    //            if (doubleArray[row, column] == doubleArray[row, i])
+    //            {
+    //              count++;
+    //            } 
+    //            Console.WriteLine($" {doubleArray[row, column]} встречается {count} раз"); 
+    //         }
+    //         }           
+    //      count = 0; 
+    // }
+
+    int num = doubleArray[row, column];
+
+    for (int i = 1; i < doubleArray.length; i++)
+    {
+        if (count > 0 && i == num)
+        {
+            выводим
+        }
+    }
+    
