d24a318f5b68fe88b157acdcdc93722462a0ac3f
Полина Брагина <polyandriys14@mail.ru>
2021-12-23 22:36:30 +0300

    task 74

---
+++ Seminar/Task 74/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int numberFirst = 4;
+int numberSecond = 7;
+int count = 12;
+
+int Fibonacci(int a, int b, int n)
+{
+if (n <= a) return a;
+if (n <= b) return b;
+else
+{
+   return Fibonacci(a, b, n - 1) + Fibonacci(a, b, n - 2);
+}
+}
+
+for (int i = 1; i <= count; i++)
+{
+    Console.Write(Fibonacci(numberFirst, numberSecond, i) + " ");
+}
+
+
+
+// int count = 12;
+
+// int Fibonacci(int n)
+// {
+// if (n <= 2)
+// return 1;
+// else
+// {
+//    return Fibonacci(n - 2) + Fibonacci(n - 1);
+// }
+// }
+
+// for (int i = 1; i < count; i++)
+// {
+//     Console.Write(Fibonacci(i) + " ");
+// }
