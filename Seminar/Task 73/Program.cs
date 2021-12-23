9bc698990379ea59ecf2ff6b2a36d88c9e6f6864
Полина Брагина <polyandriys14@mail.ru>
2021-12-23 14:13:39 +0300

    task 73

---
+++ Seminar/Task 73/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int number = 4;
+int pow = 5;
+
+int numberPow (int a, int b)
+{
+    if (b == 1)
+    return a;
+    else
+    {
+        return numberPow(a, b - 1) * a;
+    }
+}
+Console.WriteLine(numberPow(number, pow));
+
+
+// int numberPow (int a, int b)
+// {
+//     if (b == 1)
+//     return a;
+//     else if (b%2 == 0)
+//     return numberPow(a*a, b/2);
+//     else
+//     {
+//         return numberPow(a, b - 1) * a;
+//     }
+// }
+// Console.WriteLine(numberPow(number, pow));
