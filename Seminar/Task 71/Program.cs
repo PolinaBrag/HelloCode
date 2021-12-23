f1dacb3958f0193b895306db64bbc1dc76d242fd
Полина Брагина <polyandriys14@mail.ru>
2021-12-23 11:57:29 +0300

    task 71

--- Seminar/Task 71/Program.cs
+++ Seminar/Task 71/Program.cs
@@ -1,18 +1,16 @@
 ﻿// Найти сумму цифр числа
 
-int number = 15;
+int number = -365;
 
 int sumOfDigits(int num)
 {
+    if (num < 0)
+        return sumOfDigits(num * -1);
     if (num < 10)
     {
         return num;
     }
-    else
-    {
-      
-        return sumOfDigits(num/10) + num %10;
-    }
+        return sumOfDigits(num / 10) + num % 10;
 }
 
 Console.WriteLine(sumOfDigits(number));
