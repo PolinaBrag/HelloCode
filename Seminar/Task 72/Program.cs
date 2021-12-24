9b621a4e039285ebcd45940b74fc3be116e9cf36
Полина Брагина <polyandriys14@mail.ru>
2021-12-24 13:13:28 +0300

    task 72

--- Seminar/Task 72/Program.cs
+++ Seminar/Task 72/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Написать программу вычисления функции Аккермана
 
+int num1 = 3;
+int num2 = 4;
 
+int AccermanFuc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m > 0) && (n == 0))
+    {
+        return AccermanFuc(m - 1, 1);
+    }
+    else
+    {
+        return AccermanFuc(m - 1, AccermanFuc(m, n - 1));
+    }
+}
+
+Console.WriteLine(AccermanFuc(num1, num2));
